9fd391b06bcac3579f987df83e77f51a7eb0c129
mbatorowicz <mbatorowicz@gmail.com>
2026-04-09 08:59:39 +0200

    Fix: Generowanie schematów Astro (Content Collections) do parsowania zlokalizowanych plików graficznych wpisow

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+import { defineCollection, z } from 'astro:content';
+
+const newsCollection = defineCollection({
+  type: 'content',
+  schema: ({ image }) => z.object({
+    title: z.string(),
+    date: z.string(),
+    author: z.string().optional(),
+    category: z.string(),
+    categoryName: z.string().optional(),
+    coverImage: image().optional(),
+    galleryImages: z.array(image()).optional(),
+  })
+});
+
+export const collections = {
+  news: newsCollection,
+};
